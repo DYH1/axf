b29fa34e43fed4a517aa5534a29ffb810447a097
DYH1 <Duanyh189331>
2018-10-08 20:47:35 +0800

    修改settings.py

--- axf/settings.py
+++ axf/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'x%xcotzh=zf%=%5s#flmp4pa31&@w=_4=qhc)sdcfcfu(!mbfm'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
